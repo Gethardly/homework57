60f67697fda756593ffec9e834ed88121ff77dc7
Kurmanaliev Bekzhan <gethardlyever@gmail.com>
2022-11-11 09:49:10 +0600

    Created user form for add a new user with required fields

---
+++ src/Form/UserForm.tsx
@@ -0,0 +1,90 @@
+import React, {useState} from 'react';
+import './UserForm.css';
+import {UserType, UserTypeMutation} from "../types";
+
+interface FormProps {
+  onSubmit: (user: UserType) => void;
+}
+
+const UserForm: React.FC<FormProps> = ({onSubmit}) => {
+  const [user, setUser] = useState<UserTypeMutation>({
+    name: '',
+    email: '',
+    active: false,
+    role: '',
+  });
+
+  const onFormChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const {name, value} = e.target;
+
+    setUser(prev => ({...prev, [name]: value}));
+  };
+
+  const onFormSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    onSubmit({
+      id: Math.random().toString(),
+      ...user,
+    })
+  };
+
+  return (
+    <>
+      <form className="mt-2 ms-2 mainForm" onSubmit={onFormSubmit}>
+
+        <div className="form-group">
+          <input
+            required
+            name="name"
+            value={user.name}
+            type="text"
+            placeholder="Name"
+            className="form-control"
+            onChange={onFormChange}
+          />
+        </div>
+
+        <div className="form-group">
+          <input
+            required
+            name="email"
+            value={user.email}
+            type="email"
+            placeholder="E-mail"
+            className="form-control"
+            onChange={onFormChange}
+          />
+        </div>
+
+        <div className="form-group">
+          <label className="me-2">Active:</label>
+          <input
+            type="checkbox"
+            name="active"
+            onChange={(e) => {
+              setUser(prev => ({...prev, active: e.target.checked}));
+            }}
+          />
+        </div>
+
+        <div className="form-group">
+          <select
+            required
+            name="role"
+            value={user.role}
+            onChange={onFormChange}
+          >
+            <option value=''>Select a role</option>
+            <option>Administrator</option>
+            <option>Manager</option>
+            <option>CEO</option>
+          </select>
+        </div>
+        <button type="submit" className="btn btn-primary">Add new user</button>
+      </form>
+    </>
+  );
+};
+
+export default UserForm;
